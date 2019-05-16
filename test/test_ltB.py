eb162642ae9c4a43c69f72658e76cc8d443c9acb
Saatwik Kamabadkone <1601869@students.wits.ac.za>
2019-05-16 12:26:27 +0200

    FinalCommit

--- test/test_ltB.py
+++ test/test_ltB.py
@@ -1,6 +1,6 @@
 import unittest
 
-import find_exams
+import find_exams.find_exams
 
 
 class TestExclude(unittest.TestCase):
@@ -17,14 +17,11 @@ class TestExclude(unittest.TestCase):
         exams = find_exams.getExams("examlist.csv")
         self.assertEqual(exams, {'ELEN4013': ('2020-06-01', 'SHWWB'),'SCMD1003': ('2020-05-28', 'CM2'),'ELEN7039': ('2020-06-15', 'CM2'),'COMS7059': ('2020-05-23', 'FNB64'),'ELEN3002': ('2020-05-24', 'CM3'),'ELEN3003': ('2020-05-24', 'CM3'),'ELEN3008': ('2020-06-03', 'EH'),'ELEN4010': ('2020-05-29', 'TND')})
 
-
-
-
-
-
-
-
-
+    ##def test_getTimeTable(self):
+    ##   courses = find_exams.getCoursesForLects("lectlist.csv")
+    ##    exams = find_exams.getExams("examlist.csv")
+    ##    timetable = find_exams.getTimeTable(courses, exams)
+    ##    self.assertEqual(timetable, "NotSure")
 
 
 
